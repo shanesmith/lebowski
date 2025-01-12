21082a2f27d003e344d1758020741f9f62c21fcc
Shane Smith <shane.wm.smith@gmail.com>
2025-01-11 21:52:25 -0500

    wip

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,34 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const {onCall, HttpsError} = require("firebase-functions/v2/https");
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+// const logger = require("firebase-functions/logger");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+function requireAuth(request) {
+  if (request.auth) {
+    return;
+  }
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  throw new HttpsError("unauthenticated");
+}
+
+async function getUserPrefs(request) {
+  requireAuth(request);
+
+  const snapshot = await getFirestore()
+      .doc(`users/${request.auth.uid}`)
+      .get();
+
+  if (!snapshot.exists) {
+    return new HttpsError("not-found");
+  }
+
+  return snapshot.data();
+}
+
+// TODO set CORS
+exports.doTheThing = onCall(async (request) => {
+  const prefs = await getUserPrefs(request);
+  return prefs;
+});
